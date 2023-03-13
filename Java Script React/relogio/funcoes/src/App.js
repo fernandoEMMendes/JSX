419c4b010c4ec99e7f336c78b1a910f1e227183a
Fernando Mendes <fernandoeduardomottamendes@gmail.com>
2023-03-13 17:23:18 -0300

    sauce

--- Java Script React/relogio/funcoes/src/App.js
+++ Java Script React/relogio/funcoes/src/App.js
@@ -1,10 +1,19 @@
 function App() {
+  function clicar(){
+    var x = 42
+    var y = 32
+    var z = x + y 
+  }
+  
   return (
-   
-     <p> funções </p>
-  
-  
-   );
+  <div>
+    <h1> funções </h1>
+
+    <button onClick={clicar}>Somar</button>
+    <p> o resultado é: </p>
+
+  </div>
+  );
 }
 
 export default App;
