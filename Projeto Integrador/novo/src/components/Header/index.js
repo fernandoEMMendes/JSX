4798ba0ed0bd45362be742a36ff1822125944f2e
Fernando <fernandoeduardomottamendes@gmail.com>
2023-03-31 17:19:56 -0300

    sauce

--- Projeto Integrador/novo/src/components/Header/index.js
+++ Projeto Integrador/novo/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import "../css/Header.css"
   //os <a href> são links antigos que tem que ser mudado para o route, eles ainda não possuem página própria
-  //ainda falta ser feito muitas mudanças no CSS
+  //ainda falta ser feito MUITAS mudanças no CSS
   //a tabela feita no estoque, não funcionou então vamos ter que ver como fazer uma no java script react
 function Header() {
     return (
