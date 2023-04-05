bfab484642703a06058cc8b77b92ebe6d906611d
Fernando <fernandoeduardomottamendes@gmail.com>
2023-04-05 14:34:33 -0300

    sauce

---
+++ Luciano/arrays/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  const numeros = ["banana", "Laranja", "pera"]
+  numeros.push  ("Maça")
+  numeros.splice  (1, 0, "Bergamota")
+
+  console.log(numeros)
+
+
+
+}
+
+export default App;
