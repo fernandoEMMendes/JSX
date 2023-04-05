56e7cbbaee146a13fa70c853c9bbb220186a2193
Fernando <fernandoeduardomottamendes@gmail.com>
2023-04-05 15:40:23 -0300

    sauce

--- Projeto Integrador/antigo/alonedark/src/App.js
+++ Projeto Integrador/antigo/alonedark/src/App.js
@@ -1,10 +1,37 @@
-import Contador from "./componentes/contador"
-
+import React, { useState } from "react"
 function App() {
+  //Cont = Contador
+  const [frases, setfrases] = useState("")
+  const [emmaos, setemmaos] = useState("")
+  const [ContGraveto, SetContGraveto] = useState(0)
+  
+
+
+  function Frases() {
+    if (ContGraveto === 0) {
+      frases = ("Darkness... I can't see anything.")
+
+    }
+  }
+
+  function Coletar() {
+    SetContGraveto(ContGraveto + 1)
+  }
+
+  //-----------------------------------------------------------
+  //-----------------------------------------------------------
+  //-----------------------------------------------------------
+
   return (
 
     <div>
-      <Contador />
+      <span>{frases}</span> <br />
+      <p>Equip:  {emmaos}</p> <br /> <br />
+      
+      <h3>{ContGraveto}</h3>
+      <button onClick={Coletar}>Gather sticks</button> <br />
+
+
     </div>
   );
 }
