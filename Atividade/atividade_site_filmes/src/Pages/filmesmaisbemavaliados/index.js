402cb512a1e780db74fbd3eed71ed3f3f3b76832
Fernando <fernandoeduardomottamendes@gmail.com>
2023-04-18 17:21:07 -0300

    sauce

--- Atividade/atividade_site_filmes/src/Pages/filmesmaisbemavaliados/index.js
+++ Atividade/atividade_site_filmes/src/Pages/filmesmaisbemavaliados/index.js
@@ -37,17 +37,10 @@ function FilmesMaisBemAvaliados() {
                 <div class="grid-container">
                     {Filmes.map((filmes) => {
                         return (
-<<<<<<< HEAD
                             <p key={filmes.id}>
                                 <b>{filmes.title}</b> <br />
                                 <div><img src={`https://image.tmdb.org/t/p/w300/${filmes.poster_path}`} alt={filmes.original_title} /> </div>
                                 <Link to={`/Detalhes/${filmes.id}`}><h2>Detalhes</h2></Link> <br />
-=======
-                            <p key={filme.id}>
-                                <b>{filme.title}</b> <br />
-                                <div><img src={`https://image.tmdb.org/t/p/w300/${filme.poster_path}`} alt={filme.original_title} /> </div>
-                                <Link to={`/Detalhes/${filme.id}`}><h2>Detalhes</h2></Link> <br />
->>>>>>> 74dc4ee0f467fe25dd21c07fb7da08cda4ba1a38
                             </p>
                         )
                     })}
