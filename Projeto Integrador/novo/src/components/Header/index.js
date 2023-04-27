4663f2cd8d7ba9b44cf929bdb8300d3d42ad6ec5
Fernando <fernandoeduardomottamendes@gmail.com>
2023-04-27 14:16:37 -0300

    2404

--- Projeto Integrador/novo/src/components/Header/index.js
+++ Projeto Integrador/novo/src/components/Header/index.js
@@ -5,7 +5,7 @@ import "../css/Header.css"
 function Header() {
     return (
         <div id="area-cabecalho">
-
+            
             <div id="area-logo">
                 <h1> Vista-se! <span class="branco">Loja Virtual</span></h1>
             </div>
@@ -13,8 +13,8 @@ function Header() {
                 <Link to="/" class="link"><h2>Home</h2></Link>
                 <Link to="/QuemSomos" class="link"><h2>Quem Somos</h2></Link>
                 <Link to="/Loja" class="link"><h2>Loja</h2></Link>
-                <Link to="/Produtos" class="link"><h2>Produtos</h2></Link>
-                <Link to="/Contato" class="link"><h2>Contato</h2></Link>  
+                <Link to="/Produtos" class="link"><h2>Produtos</h2></Link>  
+                <Link to="/Contato" class="link"><h2>Contato</h2></Link>
             </div>
 
         </div>
--- Projeto Integrador/novo/src/pages/Produtos/index.js
+++ Projeto Integrador/novo/src/pages/Produtos/index.js
@@ -1,16 +1,29 @@
-
+import { useState } from "react"
 
 function Produtos() {
+
+    const [valor, setvalor] = useState("");
+
+    function random(min, max) {
+        return (Math.floor(1, 4) * (max - min)) + min;
+    }
+
+    if (random = 2) {
+        setvalor("Disponível")
+        return
+    } else {
+        setvalor("Indisponível")
+        return
+    }
+
+
     return (
-        <>
-            <div className="grid-container">
-                <a> Produtos </a> <br />
-                <a> Produto1 </a> <br />
-                <a> Disponível </a> <br />
-                <a> Renner </a> <br />
-            </div>
-        </>
+        <div>
+            <h3>Produto 1</h3>
+            <a>{valor}</a>
+        </div>
     )
+
 }
 
 export default Produtos
