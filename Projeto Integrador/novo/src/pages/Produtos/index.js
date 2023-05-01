14506d52ac4d8a69de6568b10e5fe677223cf3de
Fernando <fernandoeduardomottamendes@gmail.com>
2023-05-01 13:39:12 -0300

    sauce

--- Projeto Integrador/novo/src/pages/Produtos/index.js
+++ Projeto Integrador/novo/src/pages/Produtos/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import "../../components/css/produtos.css"
 
 function Produtos() {
 
@@ -6,111 +6,59 @@ function Produtos() {
 
 
     return (
-        <div>
-            <table>
-                <button onClick={function () { disponibilidade(); disponibilidade2(); disponibilidade3(); }}>Verificar disponibilidade</button>
-                <tr>
-                    <th>Nome</th>
-                    <th>Marca</th>
-                    <th>Estoque</th>
-                </tr>
+        <div className="grid-container">
+            <div className="legenda">
+                Produtos
+            </div>
+            <div className="legenda">
+                Marca
+            </div>
+            <div className="legenda">
+                Disponibilidade
+            </div>
 
-                <tr>
-                    <td>Camisetas</td>
-                    <td>Adidas</td>
-                    <td>Disponível</td>
-                </tr>
-                <tr>
-                    <td>Calças</td>
-                    <td>Nike</td>
-                    <td>Indisponível</td>
-                </tr>
-                <tr>
-                    <td>Calçados</td>
-                    <td>Mizuno</td>
-                    <td>disponivel</td>
-                </tr>
-                <tr>
-                    <td>Bonés</td>
-                    <td>Oakley</td>
-                    <td>Disponível</td>
-                </tr>
+            <div>
+                Camisetas
+            </div>
+            <div>
+                Supreme
+            </div>
+            <div className="disponivel">
+                Disponível
+            </div>
 
-            </table>
+            <div>
+                Calças
+            </div>
+            <div>
+                Nike
+            </div>
+            <div className="disponivel">
+                Disponível
+            </div>
+
+            <div>
+                Calçados
+            </div>
+            <div>
+                Mizuno
+            </div>
+            <div className="nao-disponivel">
+                Não Disponível
+            </div>
+
+            <div>
+                Acessórios
+            </div>
+            <div>
+                Oakley
+            </div>
+            <div className="disponivel">
+                Disponível
+            </div>
         </div >
     )
 
 }
 
 export default Produtos
-
-//Descobrir razão pela qual o random trava após alguns usos
-
-//const [valor, setvalor] = useState("")
-//const [valor2, setvalor2] = useState("")
-//const [valor3, setvalor3] = useState("")
-
-//function random(min, max) {
-//    return function () {
-//        return Math.floor(Math.random() * (1 + max - min)) + min;
-//    }
-//}
-
-//function random2(min, max) {
-//    return function () {
-//        return Math.floor(Math.random() * (1 + max - min)) + min;
-//    }
-//}
-
-//function random3(min, max) {
-//    return function () {
-//        return Math.floor(Math.random() * (1 + max - min)) + min;
-//    }
-//}
-
-
-
-
-//var aleatorio = random(1, 2)
-//var resultado = ""
-//for (var i = 0; i < 1; i++) {
-//    resultado += aleatorio() + "";
-//}
-
-//var aleatorio2 = random2(1, 2)
-//var resultado2 = ""
-//for (var i = 0; i < 1; i++) {
-//    resultado2 += aleatorio2() + "";
-//}
-
-//var aleatorio3 = random3(1, 2)
-//var resultado3 = ""
-//for (var i = 0; i < 1; i++) {
-//    resultado3 += aleatorio3() + "";
-//}
-
-
-
-//function disponibilidade() {
-//    if (resultado >= 2) {
-//        setvalor("Disponível")
-//    } else {
-//        setvalor("Indisponível")
-//    }
-//}
-
-//function disponibilidade2() {
-//    if (resultado >= 2) {
-//        setvalor2("Disponível")
-//    } else {
-//        setvalor2("Indisponível")
-//    }
-//}
-
-//function disponibilidade3() {
-//    if (resultado >= 2) {
-//        setvalor3("Disponível")
-//    } else {
-//        setvalor3("Indisponível")
-//    }
-//}
