c9ffb59802224be68ba531a6092e0fe22bf29908
Fernando <fernandoeduardomottamendes@gmail.com>
2023-05-12 17:10:57 -0300

    sauce

--- Projeto Integrador/claudia/src/pages/Produtos/index.js
+++ Projeto Integrador/claudia/src/pages/Produtos/index.js
@@ -3,7 +3,13 @@
 function Produtos() {
     return (
         <div>
-            Junto com nossos serviços oferecemos também
+            <a>Junto com nossos serviços oferecemos também produtos para complementar o tratamento para melhores resultados</a><br /> <br /> <br /> <br /> <br />
+            <h2>VO³</h2> <br />
+            <img src="" alt="òleo de peixe VO³" /> <br />
+            <img src="" alt="Descrição VO³" /> <br /> <br />
+            <h2>Capilarize+</h2> <br />
+            <img src="" alt="Suplemento alimentar Capilarize+" /> <br />
+            <img src="" alt="Descrição Capilarize+" /><br /> 
         </div>
     )
 }
