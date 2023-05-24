1358598a8b7d2537462c47ef1d0e2050b897c7c8
Fernando <fernandoeduardomottamendes@gmail.com>
2023-05-24 16:52:10 -0300

    sauce

--- Projeto Integrador/Pessoal/MyTest/src/Pages/Home/index.js
+++ Projeto Integrador/Pessoal/MyTest/src/Pages/Home/index.js
@@ -1,24 +1,33 @@
-import {useState, useRef} from "react"
+import { useState, useRef } from "react"
+
+import "../../css/Home.css"
 
 export default function Home() {
 
     const [vida, setvida] = useState(100)
     const [vida2, setvida2] = useState(100)
 
+    function random(min, max) {
+        return Math.floor(Math.random());
+    }
     
-    
-    
-    
-    
-    
-    
+    function attack() {
+        if (vida2 <= 0) {
+            alert("Venceu!")
+        } else { setvida2(vida2 - random(1, 5)) }
+    }
+
+
+
     return (
         <>
             <div>
                 <h1>Home</h1><br /><br />
-                <a>HP: {vida}</a> <br /><br />
+                <a>HP: {vida}</a> <br />
+                <button onClick={attack}>attack</button><br /> <br />
+                <a>(o_o)</a><br /><br />
+                <a>HP: {vida2}</a><br /><br />
                 <a>(o_o)</a><br />
-                <a></a>
             </div>
         </>
     )
