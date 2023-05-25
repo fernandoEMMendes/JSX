d62e603e9a4270f3860531c00e862489c7ce9ede
Fernando <fernandoeduardomottamendes@gmail.com>
2023-05-25 17:20:19 -0300

    sauce

--- Projeto Integrador/CabeloSite/src/pages/Servicos/index.js
+++ Projeto Integrador/CabeloSite/src/pages/Servicos/index.js
@@ -13,19 +13,19 @@ export default function Servicos (){
         <div class="embed-responsive embed-responsive-16by9"><br/>
 
         <h2 class='textoServiço'>Tricoscopio</h2>
-        <iframe class="embed-responsive-item" src={video1} width="600" height="400" allowfullscreen="" onPause={10} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
+        <iframe class="embed-responsive-item" src={video1} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
         <h2 class='textoServiço'>Silene Cassia falando sobre o Trabalho</h2>
-        <iframe class="embed-responsive-item" src={video2} width="600" height="400" allowfullscreen="" onPause={10} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
+        <iframe class="embed-responsive-item" src={video2} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
         <h2 class='textoServiço'>Nome não identificado</h2>
-        <iframe class="embed-responsive-item" src={video3} width="600" height="400" allowfullscreen=""onPause={10} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
+        <iframe class="embed-responsive-item" src={video3} width="600" height="400" allowfullscreen=""onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
         <h2 class='textoServiço'>Alta Frequência</h2>
-        <iframe class="embed-responsive-item" src={video4} width="600" height="400" allowfullscreen="" onPause={10} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
+        <iframe class="embed-responsive-item" src={video4} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
         <h2 class='textoServiço'>Laser e Led</h2>
-        <iframe class="embed-responsive-item" src={video5} width="600" height="400" allowfullscreen="" onPause={10} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
+        <iframe class="embed-responsive-item" src={video5} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
         <h2 class='textoServiço'>convite de Claudia Mendes sobre o Tricoscopio</h2>
-        <iframe class="embed-responsive-item" src={video6} width="600" height="400" allowfullscreen="" onPause={10} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
+        <iframe class="embed-responsive-item" src={video6} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
         <h2 class='textoServiço'>Nome não identificado</h2>
-        <iframe class="embed-responsive-item" src={video7} width="600" height="400" allowfullscreen=""  onPause={10} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
+        <iframe class="embed-responsive-item" src={video7} width="600" height="400" allowfullscreen=""  onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
         </div>
 
     )
