5e25041d9b2f8d464eb9a5c46e8ef588518d044a
Fernando <fernandoeduardomottamendes@gmail.com>
2023-05-30 17:09:41 -0300

    sauce

--- Projeto Integrador/CabeloSite/src/pages/Servicos/index.js
+++ Projeto Integrador/CabeloSite/src/pages/Servicos/index.js
@@ -8,24 +8,24 @@ import video6 from '../../videos/video6.mp4'
 import video7 from '../../videos/video7.mp4'
 import '../../css/Servicos.css'
 
-export default function Servicos (){
-    return(
-        <div class="embed-responsive embed-responsive-16by9"><br/>
+export default function Servicos() {
+    return (
+        <div class="embed-responsive embed-responsive-16by9"><br />
 
-        <h2 class='textoServiço'>Tricoscopio</h2>
-        <iframe class="embed-responsive-item" src={video1} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
-        <h2 class='textoServiço'>Silene Cassia falando sobre o Trabalho</h2>
-        <iframe class="embed-responsive-item" src={video2} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
-        <h2 class='textoServiço'>Nome não identificado</h2>
-        <iframe class="embed-responsive-item" src={video3} width="600" height="400" allowfullscreen=""onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
-        <h2 class='textoServiço'>Alta Frequência</h2>
-        <iframe class="embed-responsive-item" src={video4} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
-        <h2 class='textoServiço'>Laser e Led</h2>
-        <iframe class="embed-responsive-item" src={video5} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
-        <h2 class='textoServiço'>convite de Claudia Mendes sobre o Tricoscopio</h2>
-        <iframe class="embed-responsive-item" src={video6} width="600" height="400" allowfullscreen="" onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
-        <h2 class='textoServiço'>Nome não identificado</h2>
-        <iframe class="embed-responsive-item" src={video7} width="600" height="400" allowfullscreen=""  onLoad={pause} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br/>
+            <h2 class='textoServiço'>Tricoscopio</h2>
+            <iframe class="embed-responsive-item" src={video1} width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br />
+            <h2 class='textoServiço'>Silene Cassia falando sobre o Trabalho</h2>
+            <iframe class="embed-responsive-item" src={video2} width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br />
+            <h2 class='textoServiço'>Nome não identificado</h2>
+            <iframe class="embed-responsive-item" src={video3} width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br />
+            <h2 class='textoServiço'>Alta Frequência</h2>
+            <iframe class="embed-responsive-item" src={video4} width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br />
+            <h2 class='textoServiço'>Laser e Led</h2>
+            <iframe class="embed-responsive-item" src={video5} width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br />
+            <h2 class='textoServiço'>convite de Claudia Mendes sobre o Tricoscopio</h2>
+            <iframe class="embed-responsive-item" src={video6} width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br />
+            <h2 class='textoServiço'>Nome não identificado</h2>
+            <iframe class="embed-responsive-item" src={video7} width="600" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br />
         </div>
 
     )
--- Projeto Integrador/claudia/src/pages/Produtos/index.js
+++ Projeto Integrador/claudia/src/pages/Produtos/index.js
@@ -1,28 +1,33 @@
+import Carousel from "react-bootstrap/Carousel"
 
+import "./Produtos.css"
 
-import vo3 from "../../imgs/IMG-20230511-WA0020.jpg"
-import vo3texto from "../../imgs/IMG-20230511-WA0021.jpg"
-import capilarize from "../../imgs/IMG-20230511-WA0022.jpg"
-import capilarizetexto from "../../imgs/IMG-20230511-WA0023.jpg"
+import vo3 from "../../imgs/IMG-20230511-WA0020cop.jpg"
+import vo3texto from "../../imgs/IMG-20230511-WA0021cop.jpg"
+import capilarize from "../../imgs/IMG-20230511-WA0022cop.jpg"
+import capilarizetexto from "../../imgs/IMG-20230511-WA0023cop.jpg"
 
 function Produtos() {
     return (
         <>
-            <div>
-                <a className="titulo">Junto com nossos serviços oferecemos também produtos para complementar o tratamento para melhores resultados</a><br /> <br /> <br /> <br /> <br />
-            </div>
-            <div className="grid-container">
-                <div><h2 className="h2produtos">VO³</h2> <br /></div>
-                <div>
-                    <img src={vo3} width="250" alt="òleo de peixe VO³" /> <br />
-                    <img src={vo3texto} width="250" alt="Descrição VO³" /> <br /> <br />
-                </div>
-                <div><h2 className="h2produtos">Capilarize+</h2> <br /></div>
-                <div>
-                    <img src={capilarize} width="250" alt="Suplemento alimentar Capilarize+" /> <br />
-                    <img src={capilarizetexto} width="250" alt="Descrição Capilarize+" /><br />
-                </div>
-                <h1>.</h1>
+            <div className="align">
+                <Carousel className="carousel" interval="99999" variant="dark">
+                    <Carousel.Item>
+                        <img className="d-flex w-100" alt="tes" src={vo3} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-flex w-100" alt="tes" src={vo3texto} />
+                    </Carousel.Item>
+                </Carousel>
+                <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+                <Carousel className="carousel" interval="99999" variant="dark">
+                    <Carousel.Item>
+                        <img className="d-flex w-100" alt="tes" src={capilarize} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-flex w-100" alt="tes" src={capilarizetexto} />
+                    </Carousel.Item>
+                </Carousel>
             </div>
         </>
     )
