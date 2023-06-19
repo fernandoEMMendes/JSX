264c06d792f246b419bedbce8c9b5f5432c9a099
fernando mendes <fernandoeduardomottamendes@gmail.com>
2023-06-19 12:08:21 -0300

    sauces

--- Projeto Integrador/claudia/src/pages/QuemSomos/index.js
+++ Projeto Integrador/claudia/src/pages/QuemSomos/index.js
@@ -3,7 +3,11 @@
 function QuemSomos() {
     return (
         <div>
-            Essa é a QuemSomos
+            <h1>As profissionais do salão Claudia Mendes:</h1>
+            <br /> <br />
+            <h2>Claudia Mendes</h2>
+            <br />
+            <h2>Cilene Cassia</h2>
         </div>
     )
 }
