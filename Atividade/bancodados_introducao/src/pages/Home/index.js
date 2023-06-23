49534821ef6dc03e5ff181fdfdeb3af3420ec7eb
Fernando <fernandoeduardomottamendes@gmail.com>
2023-06-23 16:20:08 -0300

    sat

--- Atividade/bancodados_introducao/src/pages/Home/index.js
+++ Atividade/bancodados_introducao/src/pages/Home/index.js
@@ -3,36 +3,71 @@
 function Home() {
     return (
         <div>
-            <form>
-                <label >Nome Completo</label> <br />
-                <input type="text" name="nome" /> <br />
+            <div>
+                <h1>Cliente</h1>
+                <form>
+                    <label >Nome Completo</label> <br />
+                    <input type="text" name="nome" /> <br />
 
-                <label>Telefone Fixo</label> <br />
-                <input type="text" name="tel_fixo" /> <br />
- 
-                <label>Telefone Celular</label> <br />
-                <input type="text" name="tel_cel" /> <br />
+                    <label>Telefone Fixo</label> <br />
+                    <input type="text" name="tel_fixo" /> <br />
 
-                <label>Rua</label> <br />
-                <input type="text" name="rua" /> <br />
+                    <label>Telefone Celular</label> <br />
+                    <input type="text" name="tel_cel" /> <br />
 
-                <label>complemento</label> <br />
-                <input type="text" name="complemento" /> <br />
+                    <label>Rua</label> <br />
+                    <input type="text" name="rua" /> <br />
 
-                <label>cep</label> <br />
-                <input type="text" name="cep" /> <br />
+                    <label>complemento</label> <br />
+                    <input type="text" name="complemento" /> <br />
 
-                <label>bairro</label> <br />
-                <input type="text" name="bairro" /> <br />
+                    <label>cep</label> <br />
+                    <input type="text" name="cep" /> <br />
 
-                <label>cidade</label> <br />
-                <input type="text" name="cidade" /> <br />
+                    <label>bairro</label> <br />
+                    <input type="text" name="bairro" /> <br />
 
-                <label>estado</label> <br />
-                <input type="text" name="estado" /> <br />
+                    <label>cidade</label> <br />
+                    <input type="text" name="cidade" /> <br />
 
-                <button type="submit">submit</button>
-            </form>
+                    <label>estado</label> <br />
+                    <input type="text" name="estado" /> <br />
+
+                    <button type="submit">submit</button>
+                </form>
+            </div>
+
+            <h1>Pessoa Fisica</h1>
+            <div>
+                <form>
+                    <label>cpf</label> <br />
+                    <input type="text" name="cpf" /> <br />
+
+                    <label>rg</label> <br />
+                    <input type="text" name="rg" /> <br />
+
+                    <label>Data Nascimento</label> <br />
+                    <input type="text" name="data_nas" /> <br />
+
+                    <button type="submit">submit</button>
+                </form>
+            </div>
+
+            <h1>Pessoa Juridica</h1>
+            <div>
+                <form>
+                    <label>cnpj</label> <br />
+                    <input type="text" name="cnpj" /> <br />
+
+                    <label>ie</label> <br />
+                    <input type="text" name="ie" /> <br />
+
+                    <label>situação cadastral</label> <br />
+                    <input type="text" name="sit_cadastro" /> <br />
+
+                    <button type="submit">submit</button>
+                </form>
+            </div>
         </div>
     )
 }
