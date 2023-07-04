03267384f6b69b5f971d4b5496f6a86bb3b85543
Fernando Mendes <fernandoeduardomottamendes@gmail.com>
2023-07-04 12:40:50 -0300

    sauce

--- Projeto Integrador/pi_fernando/src/pages/Home/index.js
+++ Projeto Integrador/pi_fernando/src/pages/Home/index.js
@@ -1,8 +1,13 @@
-
+import demo from "../../imgs/demoman 4k.jpeg"
 
 
 export default function Home() {
-    return(
-        <h1>Home</h1>
+    return (
+        <>
+
+            <h1>Home</h1> <br />
+            <img className="img-fluid rounded" width="300" src={demo} alt="demoman tf2 in 4K"/>
+
+        </>
     )
 }
