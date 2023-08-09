aeba333ac14651bcaf0b54eeeb07045dc5fd48f0
Fernando <fernandoeduardomottamendes@gmail.com>
2023-08-09 16:53:51 -0300

    09/08/2023

--- Atividade/bancodado2/src/services/Usuarios/CriarUsuarioServices.ts
+++ Atividade/bancodado2/src/services/Usuarios/CriarUsuarioServices.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma"
+
 
 interface CriarUsuario{
     nome : string
@@ -7,8 +9,18 @@ interface CriarUsuario{
 
 class CriarUsuarioService{
     async execute({nome, email, senha} : CriarUsuario){
+        if (!nome || !email || !senha)
+        throw new Error("Há campos em brancos!")
 
+        const emailJaCadastrado = await prismaClient.user.findFirst({
+            where:{
+                email : email
+            }
+        })
 
+        if(emailJaCadastrado){
+            throw new Error("E-mail já cadastrado!")
+        }
     }
 }
 
