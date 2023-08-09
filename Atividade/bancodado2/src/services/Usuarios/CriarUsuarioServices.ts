56b27e52d54dd5ea42fd93447263389f663853cd
Fernando <fernandoeduardomottamendes@gmail.com>
2023-08-09 17:26:42 -0300

    09/08/2023

--- Atividade/bancodado2/src/services/Usuarios/CriarUsuarioServices.ts
+++ Atividade/bancodado2/src/services/Usuarios/CriarUsuarioServices.ts
@@ -1,28 +1,42 @@
 import prismaClient from "../../prisma"
 
 
-interface CriarUsuario{
-    nome : string
-    email : string
-    senha : string
+interface CriarUsuario {
+    nome: string
+    email: string
+    senha: string
 }
 
-class CriarUsuarioService{
-    async execute({nome, email, senha} : CriarUsuario){
+class CriarUsuarioService {
+    async execute({ nome, email, senha }: CriarUsuario) {
         if (!nome || !email || !senha)
-        throw new Error("Há campos em brancos!")
+            throw new Error("Há campos em brancos!")
 
         const emailJaCadastrado = await prismaClient.user.findFirst({
-            where:{
-                email : email
+            where: {
+                email: email
             }
         })
 
-        if(emailJaCadastrado){
+        if (emailJaCadastrado) {
             throw new Error("E-mail já cadastrado!")
         }
+
+        const usuario = await prismaClient.user.create({
+            data: {
+                nome: nome,
+                email: email,
+                senha: senha
+            },
+            select:{
+                id:true,
+                nome:true,
+                email:true
+            }
+        })
+        return {dados: usuario}
     }
 }
 
 
-export {CriarUsuarioService}
+export { CriarUsuarioService }
