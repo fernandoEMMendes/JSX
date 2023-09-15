999ccc131256a0ad832bfa25f011615817d9d098
Fernando <fernandoeduardomottamendes@gmail.com>
2023-09-15 13:58:53 -0300

    dddd

--- Luciano/bancodados2/frontend/src/pages/Usuarios/AlterarUsuario/index.js
+++ Luciano/bancodados2/frontend/src/pages/Usuarios/AlterarUsuario/index.js
@@ -1,10 +1,14 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import apiBack from "../../../services/apiBack"
+import "./AlterarUsuario.css"
 
 export default function AlterarUsuario() {
     const { id } = useParams()
     const [ListarUsuario, setListarUsuario] = useState("")
+
+    const [nome, setnome] = useState("")
+    const [email, setemail] = useState("")
 
     useEffect(() => {
         async function ListarUsuarioUnico() {
@@ -12,13 +16,30 @@ export default function AlterarUsuario() {
             setListarUsuario(response.data)
         }
         ListarUsuarioUnico()
-    }, [])
+    }, [id])
+
+    useEffect(() => {
+        setnome(ListarUsuario.nome)
+        setemail(ListarUsuario.email)
+    }, [ListarUsuario])
 
     return (
-        <div>
+        <div className="align">
             <h1>Editar</h1>
-            <h3>{ListarUsuario.nome}</h3>
-            <h3>{ListarUsuario.email}</h3>
+            <form>
+
+                <label>Nome: </label>
+                <input type="text" value={nome} onChange={(e) => setnome(e.target.value)} />
+
+                <br />
+
+                <label>Email: </label>
+                <input type="text" value={email} onChange={(e) => setemail(e.target.value)} />
+
+                <br />
+
+                <button type="submit">submit</button>
+            </form>
         </div>
     )
 }
