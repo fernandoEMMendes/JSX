23094a4ef8f509f94983e3b507751d63eaff7127
Fernando <fernandoeduardomottamendes@gmail.com>
2023-10-25 15:58:03 -0300

    25/10/2023

--- Luciano/UC9/frontend/src/pages/Produtos/index.js
+++ Luciano/UC9/frontend/src/pages/Produtos/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import apiLocal from "../../apiBack/apiLocal/api";
+import {toast} from "react-toastify"
 import "./Produtos.scss"
 
 export default function Produtos() {
@@ -9,27 +10,48 @@ export default function Produtos() {
     const [quantidade, setQuantidade] = useState("")
     const [preco, setPreco] = useState("")
     const [idCategoria, setIdcategoria] = useState("")
+    const [banner, setBanner] = useState(null)
 
-    const [categoria, setCategoria] = useState([""])
+    const [listarCategoria, setListarCategoria] = useState([""])
 
     useEffect(() => {
         async function verCategorias() {
             const response = await apiLocal.get("/ListarCategoria")
-            setCategoria(response.data)
+            setListarCategoria(response.data)
         }
         verCategorias()
-    }, [categoria]);
+    }, [listarCategoria]);
 
-    function Criar(e) {
-        e.preventDefault()
-
-        if (!nome || !fabricante || !quantidade || !preco || !idCategoria) {
-            alert("Campos em branco")
+    function handleImg(e) {
+        if (!e.target.files) {
+            alert("imagem faltando")
             return
         }
-        console.log(nome, fabricante, quantidade, preco, idCategoria)
+        const imagem = e.target.files[0]
+        if (imagem.type === "image/png") {
+            setBanner(imagem)
+        }
     }
 
+    async function Criar(e) {
+        try {
+            e.preventDefault(e)
+            const categoryId = idCategoria
+            const data = new FormData()
+
+            data.append("nome", nome)
+            data.append("fabricante", fabricante)
+            data.append("quantidade", quantidade)
+            data.append("file", banner)
+            data.append("preco", preco)
+            data.append("categoryId", categoryId)
+
+            const response = await apiLocal.post("/CriarProduto", data)
+            toast.success(response.data.dados)
+        } catch (err) {
+            if(err = 401) {toast.error("Campos em branco ou inválidos")}
+        }
+    }
 
     return (
         <div>
@@ -40,27 +62,48 @@ export default function Produtos() {
             <div>
                 <form className="forms" onSubmit={Criar}>
                     <label>Nome</label>
-                    <input type="text" value={nome} onChange={(e) => { setNome(e.target.value) }} />
+                    <input
+                        type="text"
+                        value={nome}
+                        onChange={(e) => { setNome(e.target.value) }}
+                    />
 
                     <label>Fabricante</label>
-                    <input type="text" value={fabricante} onChange={(e) => { setFabricante(e.target.value) }} />
+                    <input
+                        type="text"
+                        value={fabricante}
+                        onChange={(e) => { setFabricante(e.target.value) }}
+                    />
 
                     <label>Quantidade</label>
-                    <input type="text" value={quantidade} onChange={(e) => { setQuantidade(e.target.value) }} />
+                    <input
+                        type="text"
+                        value={quantidade}
+                        onChange={(e) => { setQuantidade(e.target.value) }}
+                    />
 
                     <label>Preço</label>
-                    <input type="text" value={preco} onChange={(e) => { setPreco(e.target.value) }} />
+                    <input
+                        type="text"
+                        value={preco}
+                        onChange={(e) => { setPreco(e.target.value) }}
+                    />
 
                     <label>Banner</label>
-                    <input type="file" />
+                    <a>(Somente PNGs)</a>
+                    <input
+                        type="file"
+                        accept="image/png"
+                        onChange={handleImg}
+                    />
 
                     <label>Categoria</label>
-                    <select value={idCategoria} onChange={(e) => { setIdcategoria(e.target.value) }}>
+                    <select onChange={(e) => { setIdcategoria(e.target.value) }}>
                         <option value="" id="">{">Selecione um<"}</option>
-                        {categoria.map((id) => {
+                        {listarCategoria.map((tm) => {
                             return (
-                                <option value={id.id} id={id.id}>
-                                    {`>${id.nome}<`}
+                                <option value={tm.id} id={tm.id}>
+                                    {`>${tm.nome}<`}
                                 </option>
                             )
                         })}
