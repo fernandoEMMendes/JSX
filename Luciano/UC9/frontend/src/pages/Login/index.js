35a91b9fdba2c22420ac862764ee1b775102ada8
Cappucino <fernandoeduardomottamendes@gmail.com>
2023-11-06 17:25:30 -0300

    06/11/2023

--- Luciano/UC9/frontend/src/pages/Login/index.js
+++ Luciano/UC9/frontend/src/pages/Login/index.js
@@ -15,23 +15,30 @@ export default function Login() {
 
     async function handleLogin(e) {
         e.preventDefault(e)
-        
-        let data = {
-            email,
-            password
-        }
-        const response = await signIn(data)
 
-        if (!response) {
-            toast.error("Erro de login")
-            return
-            
-            //status 200 = OK
-        } else if (response.status === 200) {
+        if (!email || !password) {
+            toast.warning("Campos obrigatorios em branco!")
+        }
+
+        try {
+            let data = {
+                email,
+                password
+            }
+            const response = await signIn(data)
+
+            if (!response) {
+                toast.error("Erro de login")
+                return
+
+                //status 200 = OK
+            }
             localStorage.setItem("@tklogin2023", JSON.stringify(response.data.token))
             toast.success("Login efetuado com sucesso")
             navigation("/Dashboard")
             return
+        } catch (err) {
+            toast.error("Email ou senha incorretos!")
         }
     }
 
