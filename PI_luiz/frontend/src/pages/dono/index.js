c75c18f0c2630845743ab3ac88876d551044f9cf
Cappucino <fernandoeduardomottamendes@gmail.com>
2023-11-09 17:23:45 -0300

    09/11/2023

--- PI_luiz/frontend/src/pages/dono/index.js
+++ PI_luiz/frontend/src/pages/dono/index.js
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import './dono.scss'
 
-export default function Donos(){
-    return(
-        <div className="conteinerEscolha">
-          <p>Você é :</p>
-          <button><Link to='/Home'>Clientes</Link></button>
-          <button><Link to='/Login'>Administrador</Link></button>
-        </div>
-    )
+export default function Donos() {
+
+  const navigation = useNavigate()
+
+  function handleCliente () {
+    navigation("/Home")
+  }
+
+  function handleAdmin () {
+    navigation("/Login")
+  }
+
+  return (
+    <div>
+      <div className="conteinerTitulo">
+        <p>Você é :</p>
+      </div>
+
+      <div className="conteinerEscolha">
+        <button onClick={handleCliente}>
+          <Link>Clientes</Link>
+        </button>
+
+        <button onClick={handleAdmin}>
+          <Link>Administrador</Link>
+        </button>
+      </div>
+    </div>
+  )
 }
