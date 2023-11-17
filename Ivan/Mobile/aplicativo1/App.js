116d3c9b620e60142f51184178a8fe8445ee4afd
Cappucino <fernandoeduardomottamendes@gmail.com>
2023-11-17 17:17:38 -0300

    17/11/2023

--- Ivan/Mobile/aplicativo1/App.js
+++ Ivan/Mobile/aplicativo1/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Palmeiras não tem mundial!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F2D2BD',
     alignItems: 'center',
     justifyContent: 'center',
   },
