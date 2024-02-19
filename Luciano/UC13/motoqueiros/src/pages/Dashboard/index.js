d2be7c9c541b9ab88ef515a078335b24183c2ef2
Cappucino <fernandoeduardomottamendes@gmail.com>
2024-02-19 16:37:29 -0300

    19/02/2024

--- Luciano/UC13/motoqueiros/src/pages/Dashboard/index.js
+++ Luciano/UC13/motoqueiros/src/pages/Dashboard/index.js
@@ -7,6 +7,72 @@ import GifImage from '@lowkey/react-native-gif';
 export default function Dashboard() {
 
     const navigation = useNavigation()
+    const [user, setUser] = useState('')
+    const [id, setId] = useState('')
+    const [localizacao, setLocalizacao] = useState(null)
+
+
+    async function handleInicio() {
+        await AsyncStorage.clear()
+        navigation.navigate('Inicio')
+    }
+
+    useEffect(() => {
+        async function dados() {
+            await firebase.database().ref('usuarios').set('nome')
+        }
+        dados()
+    }, [])
+
+    useEffect(() => {
+        async function requisitarLocal() {
+            const { granted } = await requestForegroundPermissionsAsync()
+            if (granted) {
+                const positionAtual = await getCurrentPositionAsync()
+                setLocalizacao(positionAtual)
+            }
+        }
+        requisitarLocal()
+    }, [])
+
+
+
+    useEffect(() => {
+        async function loadNome() {
+            const iNome = await AsyncStorage.getItem('@nome')
+            const nome = JSON.parse(iNome)
+            setUser(nome)
+        }
+        loadNome()
+    }, [])
+
+    useEffect(() => {
+        async function loadID() {
+            const iID = await AsyncStorage.getItem('@id')
+            const Id = JSON.parse(iID)
+            setId(Id)
+        }
+        loadID()
+    }, [])
+
+    async function botaoFireBase() {
+
+        const RecuperadoId = await AsyncStorage.getItem('@id')
+        const recId = JSON.parse(RecuperadoId)
+
+        let usuarios = await firebase.database().ref('motoqueiros').child(recId)
+        let chave = usuarios.push().key
+
+        usuarios.child(chave).set({
+            id: id,
+            nome: user,
+            latitude: localizacao.coords.latitude,
+            longitude: localizacao.coords.longitude
+        })
+        alert('Dados enviados ao FireBase!')
+
+        Keyboard.dismiss()
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -14,7 +80,7 @@ export default function Dashboard() {
 
                 <View>
                     <TouchableOpacity>
-                        <Text style={styles.botao} onPress={() => { navigation.navigate("login") }}>Voltar</Text>
+                        <Text style={styles.botao} onPress={handleInicio}>Voltar</Text>
                     </TouchableOpacity>
                 </View>
 
@@ -22,8 +88,19 @@ export default function Dashboard() {
 
                 <View>
                     <Text style={styles.titulo}>Dashboard</Text>
+                    <Text style={styles.textDadosNome}>Motoqueiro: {user}</Text>
                     <Text style={styles.subTitulo}>Pedido Selecionados</Text>
                     <Text style={styles.subTitulo}>Pedido Disponíveis</Text>
+                    <TouchableOpacity
+                        style={styles.buttonFirebase}
+                        onPress={botaoFireBase}>
+                        <Text style={styles.textButtonFire}>iniciar Rota</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.buttonInicio}
+                        onPress={handleInicio}>
+                        <Text style={styles.textButtonInicio}>Limpar dados</Text>
+                    </TouchableOpacity>
                 </View>
 
                 <View style={styles.gifCaixa}>
