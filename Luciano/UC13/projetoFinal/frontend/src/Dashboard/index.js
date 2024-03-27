69a49b3dcd8cc1e5a306622456c5c29e8fd53bba
Rodrigo Shiguemitsu <117852664+rodrigoshiguemitsu@users.noreply.github.com>
2024-03-27 15:47:12 -0300

    ...

--- Luciano/UC13/projetoFinal/frontend/src/Dashboard/index.js
+++ Luciano/UC13/projetoFinal/frontend/src/Dashboard/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import apiLocal from '../API/apiLocal/api'
-
+import './Dashboard.scss'
 
 export default function Dashboard() {
     const navigation = useNavigate()
@@ -12,16 +12,25 @@ export default function Dashboard() {
     }
 
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <main id='Main_Dashboard'>
+            <header id='Header_Dashboard'>
+                <h1>Dashboard</h1>
+            </header>
 
-            <Link to='/Produtos'>Cadastrar Produtos</Link>
+            <section id='Sessao_Dashboard'>
+
+            <Link className='Links_Dashboard' to='/Produtos'><p>Cadastrar Produtos</p></Link>
             <br />
-            <Link to="/PedidosListar">Listar Pedidos Cozinha</Link>
+            <Link className='Links_Dashboard' to="/PedidosListar"><p>Listar Pedidos Cozinha</p></Link>
             <br />
-            <Link to="/PedidosCriar">Criar Pedidos Balcão</Link>
+            <Link className='Links_Dashboard' to="/PedidosCriar"><p>Criar Pedidos Balcão</p></Link>
             <br />
             <button onClick={handleSair}>Sair</button>
-        </div>
+            
+            </section>
+            <footer id='Footer_Dashboard'>
+
+            </footer>
+        </main>
     )
 }
