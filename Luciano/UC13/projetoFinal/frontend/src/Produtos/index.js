c90705d1c8692518877e3ca92d27c52e70765bb2
Yemuri note <yemuri.teodoro@gmail.com>
2024-03-28 16:48:43 -0300

    criação categoria no frontend

--- Luciano/UC13/projetoFinal/frontend/src/Produtos/index.js
+++ Luciano/UC13/projetoFinal/frontend/src/Produtos/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Modal from 'react-modal'
 import { toast } from 'react-toastify'
 import apiLocal from '../API/apiLocal/api'
 import './produtos.estilo.scss'
@@ -7,17 +8,20 @@ import './produtos.estilo.scss'
 export default function Produtos() {
 
     const [categorias, setCategorias] = useState([''])
-    const [nome, setNome] = useState('')
+    const [nomeProd, setNomeProd] = useState('')
     const [fabricante, setFabricante] = useState('')
     const [quantidade, setQuantidade] = useState('')
     const [preco, setPreco] = useState('')
+    const [criarCategoria, setCriarCategoria] = useState('')
+
+    const [modalAberto, setModalAberto] = useState(false)
 
     const [idCategoria, setIdCategoria] = useState('')
     const [imagem, setImagem] = useState(null)
 
     const iToken = localStorage.getItem('@tklogin2023')
     const token = JSON.parse(iToken)
-    
+
     useEffect(() => {
         async function loadCategorias() {
             const resposta = await apiLocal.get('/ListarCategorias', {
@@ -30,12 +34,42 @@ export default function Produtos() {
         loadCategorias()
     }, [categorias])
 
-    function handleImagem(e){
-        if(!e.target.files){
+
+    async function handleCriarCategoria(e) {
+        try {
+            e.preventDefault()
+            setCriarCategoria('')
+            const nome = criarCategoria
+            await apiLocal.post('/CriarCategorias', {
+                nome
+            }, {
+                headers: {
+                    Authorization: 'Bearer ' + `${token}`
+                }
+            })
+            toast.success('Categoria Cadastrada', {
+                toastId: 'toastId'
+            })
+        } catch (err) {
+
+        }
+    }
+
+    async function abrirModal() {
+        setModalAberto(true)
+    }
+
+    async function fecharModal() {
+        setModalAberto(false)
+    }
+
+
+    function handleImagem(e) {
+        if (!e.target.files) {
             return
         }
         const image = e.target.files[0]
-        if(image.type === 'image/png' || image.type === 'image/jpeg'){
+        if (image.type === 'image/png' || image.type === 'image/jpeg') {
             setImagem(image)
         }
     }
@@ -45,7 +79,7 @@ export default function Produtos() {
             const categoriaId = idCategoria
             const data = new FormData()
 
-            data.append('nome', nome)
+            data.append('nome', nomeProd)
             data.append('fabricante', fabricante)
             data.append('quantidade', quantidade)
             data.append('preco', preco)
@@ -53,7 +87,7 @@ export default function Produtos() {
             data.append('file', imagem)
 
             const resposta = await apiLocal.post('/CriarProdutos', data, {
-                
+
             })
             toast.success(resposta.data.dados)
 
@@ -61,7 +95,7 @@ export default function Produtos() {
             console.log(err)
         }
 
-        setNome('')
+        setNomeProd('')
         setFabricante('')
         setQuantidade('')
         setPreco('')
@@ -72,61 +106,74 @@ export default function Produtos() {
         <div className="conteinerProdutosCadastro">
             <header id='Header_Produtos'>
                 <h1>Produtos</h1>
+                
+                <button onClick={abrirModal}>Criar Categorias</button>
+                <Modal isOpen={modalAberto}>
+                    <h1>Criar Categorias</h1>
+                    <form onSubmit={handleCriarCategoria}>
+                        <input
+                        type='text'
+                        value={criarCategoria}
+                        onChange={(e) => setCriarCategoria(e.target.value)}/>
+                        <button type='submit'>Criar</button>
+                    </form>
+                    <button onClick={fecharModal}>Fechar</button>
+                </Modal>
             </header>
             <main id='Main_Produtos'>
-            <div>
-                <form onSubmit={handleCadastrar}>
-                    <select
-                    id='Select_Produto'
-                        value={idCategoria}
-                        onChange={(e) => setIdCategoria(e.target.value)}
-                    >
-                        <option >Selecione...</option>
-                        {categorias.map((item) => {
-                            return (
-                                <option
-                                    value={item.id}>
-                                    {item.nome}
-                                </option>
-                            )
-                        })}
-                    </select>
-                    <label>Nome:</label>
-                    <input
-                        type="text"
-                        value={nome}
-                        onChange={(e) => setNome(e.target.value)}
-                    />
-                    <label>Fabricante:</label>
-                    <input
-                        type="text"
-                        value={fabricante}
-                        onChange={(e) => setFabricante(e.target.value)}
-                    />
-                    <label>Quantidade:</label>
-                    <input
-                        className='Input_Number_Produtos'
-                        type="number"
-                        value={quantidade}
-                        onChange={(e) => setQuantidade(e.target.value)}
-                    />
-                    <label>Preço:</label>
-                    <input
-                        className='Input_Number_Produtos'
-                        type="number"
-                        value={preco}
-                        onChange={(e) => setPreco(e.target.value)}
-                    />
-                    <label>Imagem:</label>
-                    <input
-                        type="file"
-                        value={setImagem}
-                        accept='image/jpeg, image/png'
-                        onChange={handleImagem}
-                    />                   
-                    <button type='submit'>Enviar</button>
-                </form>
-            </div>
+                <div>
+                    <form onSubmit={handleCadastrar}>
+                        <select
+                            id='Select_Produto'
+                            value={idCategoria}
+                            onChange={(e) => setIdCategoria(e.target.value)}
+                        >
+                            <option >Selecione...</option>
+                            {categorias.map((item) => {
+                                return (
+                                    <option
+                                        value={item.id}>
+                                        {item.nome}
+                                    </option>
+                                )
+                            })}
+                        </select>
+                        <label>Nome:</label>
+                        <input
+                            type="text"
+                            value={nomeProd}
+                            onChange={(e) => setNomeProd(e.target.value)}
+                        />
+                        <label>Fabricante:</label>
+                        <input
+                            type="text"
+                            value={fabricante}
+                            onChange={(e) => setFabricante(e.target.value)}
+                        />
+                        <label>Quantidade:</label>
+                        <input
+                            className='Input_Number_Produtos'
+                            type="number"
+                            value={quantidade}
+                            onChange={(e) => setQuantidade(e.target.value)}
+                        />
+                        <label>Preço:</label>
+                        <input
+                            className='Input_Number_Produtos'
+                            type="number"
+                            value={preco}
+                            onChange={(e) => setPreco(e.target.value)}
+                        />
+                        <label>Imagem:</label>
+                        <input
+                            type="file"
+                            value={setImagem}
+                            accept='image/jpeg, image/png'
+                            onChange={handleImagem}
+                        />
+                        <button type='submit'>Enviar</button>
+                    </form>
+                </div>
             </main>
             <footer id='Footer_Produtos'></footer>
         </div>
